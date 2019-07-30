56e3740d5ceb6705e3d488478a079fcbc3aa6a96
Ian Vaernet <ianalexisnet@hotmail.com>
2019-07-29 23:42:33 -0300

    Removido el botón de menú en el footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -119,7 +119,6 @@ if(!isset($_SESSION)){
                     <!-- Footer Menu -->
                     <div class="footer_menu">
                         <nav class="navbar navbar-expand-lg justify-content-end">
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#footerNavContent" aria-controls="footerNavContent" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
                             <div class="collapse navbar-collapse" id="footerNavContent">
                                 <ul class="navbar-nav ml-auto">
                                     <li class="nav-item active">
